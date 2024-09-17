d62a72346f821ef86da3e82620b35b386f8d60e5
Allan <allanvitor.marques@outlook.com>
2024-09-17 12:54:36 -0300

    Calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #1F64B4;
+        border: 0px solid;
+        float: right;
+        clear: both;
+        padding: 0 10px;
+        border-radius: 5px 5px 5px 5px ;
+
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF
+
+    }
+`
